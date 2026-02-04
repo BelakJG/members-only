a82910306db5eaa4396b7ea9901f0bc5346c5d9c
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-02-04 12:25:24 -0500

    Added new post logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,25 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :index ]
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def index
     @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
